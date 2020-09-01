e467e0fd1f9e699dcec30de191614c51db18c554
eikyuu <v.duguet@laposte.net>
2020-09-01 15:53:19 +0200

    unique image position

--- src/components/About.js
+++ src/components/About.js
@@ -1,17 +1,23 @@
 import React from "react";
+import montre7 from "../files/montre7.jpg";
 
 const About = (props) => {
   return (
-    <section className="container about">
+    <section className="about">
       <div className="row about_div">
-        <h1 className="about_h1">About</h1>
-        <p className="about_p">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi nemo
-          necessitatibus ea nostrum esse? Quaerat officiis vitae eveniet soluta,
-          ex, inventore exercitationem, vel dignissimos facilis fugiat
-          perferendis velit quas expedita!
-        </p>
-        <button className="btn">LEARN MORE</button>
+        <h1 className="about_h1 col-sm-12 mt-5">About us</h1>
+
+        <img className="about_img col-sm-12 mt-3" src={montre7} alt="" />
+        <div className="div_about_p">
+          <p className="about_p col-sm-12 mt-3">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi nemo
+            necessitatibus ea nostrum esse? Quaerat officiis vitae eveniet
+            soluta, ex, inventore exercitationem, vel dignissimos facilis fugiat
+            perferendis velit quas expedita!
+          </p>
+        </div>
+
+        <button className="btn about_btn">LEARN MORE</button>
       </div>
     </section>
   );
